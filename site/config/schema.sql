e658ace2326ba6197c66f28ff07ce298146dee56
Robert Gerlach <info@robertgerlach.net>
2024-10-09 22:54:54 +0200

    new schema.sql

--- site/config/schema.sql
+++ site/config/schema.sql
@@ -1,6 +1,6 @@
 # SQL Export
 # Created by Querious (400074)
-# Created: 29. September 2024 at 23:54:06 CEST
+# Created: 9. October 2024 at 22:54:34 CEST
 # Encoding: Unicode (UTF-8)
 
 
@@ -16,11 +16,39 @@ SET TIME_ZONE = '+00:00';
 
 
 
+CREATE TABLE `cache` (
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `movies` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `slug` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `release_date` date DEFAULT NULL,
+  `start_datetime` datetime DEFAULT NULL,
+  `duration` int DEFAULT NULL,
+  `imdb_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=106 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `options` (
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
 CREATE TABLE `toots` (
-  `id` bigint unsigned NOT NULL,
+  `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `data` json DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
@@ -37,4 +65,4 @@ SET TIME_ZONE = @ORIG_TIME_ZONE;
 
 
 
-#  Export Finished: 29. September 2024 at 23:54:06 CEST
+#  Export Finished: 9. October 2024 at 22:54:34 CEST
