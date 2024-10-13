3f0342a4f6219d137fb19fcfca3401c1e7e47427
Robert Gerlach <info@robertgerlach.net>
2024-10-13 17:09:45 +0200

    test

--- site/src/views/parts/html-header.php
+++ site/src/views/parts/html-header.php
@@ -10,7 +10,6 @@ $filemtimeJS = ViewHelper::filemtime('js/main.js');
     <title>#monsterdon replay <?= $title ? '&middot; ' . h($title) : '';?></title>
     <link rel="stylesheet" type="text/css" href="/css/main.css?v=<?php echo $filemtimeCSS;?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="theme-color" content="#131415"/>
     <script src="/js/main.min.js?v=<?php echo $filemtimeJS;?>"></script>
     <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
     <link rel="apple-touch-icon" sizes="256x256" href="/img/icon-256.png">
