79857eaaf7b734e6ca280ac3a61e5a6184f814e7
Robert Gerlach <info@robertgerlach.net>
2024-10-17 18:59:56 +0200

    meta description, title

--- site/src/views/parts/html-header.php
+++ site/src/views/parts/html-header.php
@@ -7,10 +7,11 @@ $filemtimeJS = ViewHelper::filemtime('js/main.js');
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>#monsterdon replay <?= $title ? '&middot; ' . h($title) : '';?></title>
+    <title>monsterdon replay <?= $title ? '&middot; ' . h($title) : '';?></title>
     <link rel="stylesheet" type="text/css" href="/css/main.css?v=<?php echo $filemtimeCSS;?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#131415"/>
+    <meta name="description" content="Missed the #monsterdon watch party on Mastodon? Replay the toots here.">
     <script src="/js/main.min.js?v=<?php echo $filemtimeJS;?>"></script>
     <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
     <link rel="apple-touch-icon" sizes="256x256" href="/img/icon-256.png">
