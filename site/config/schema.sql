21fbacd0d7581dd7360a78956eda327a1282c067
Robert Gerlach <info@robertgerlach.net>
2024-10-24 23:09:04 +0200

    updated schema.sql

--- site/config/schema.sql
+++ site/config/schema.sql
@@ -1,6 +1,6 @@
 # SQL Export
 # Created by Querious (400074)
-# Created: 19. October 2024 at 15:59:07 CEST
+# Created: 24. October 2024 at 23:08:45 CEST
 # Encoding: Unicode (UTF-8)
 
 
@@ -36,7 +36,7 @@ CREATE TABLE `movies` (
   `toot_count` int DEFAULT '0',
   `og_image_cover_offset` int DEFAULT '50',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=112 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=133 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
 
 CREATE TABLE `options` (
@@ -47,11 +47,11 @@ CREATE TABLE `options` (
 
 
 CREATE TABLE `toots` (
-  `id` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `id` char(64) COLLATE utf8mb4_unicode_520_ci NOT NULL,
   `data` json DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `id` (`id`) USING BTREE
+  KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
 
@@ -68,4 +68,4 @@ SET TIME_ZONE = @ORIG_TIME_ZONE;
 
 
 
-#  Export Finished: 19. October 2024 at 15:59:07 CEST
+#  Export Finished: 24. October 2024 at 23:08:45 CEST
